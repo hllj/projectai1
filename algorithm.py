8c5f00872b8fb6623c20ccdbac3e79e9ceba5e8a
hllj <vanhop3499@gmail.com>
2019-10-01 15:06:53 +0700

    finish bfs and restruct files

---
+++ algorithm.py
@@ -0,0 +1,27 @@
+from environment import Environment
+WMAX = 1e3
+dx = [-1, 0, 1, 1, 1, 0, -1, -1]
+dy = [1, 1, 1, 0, -1, -1, -1, 0]
+
+class Algorithm():
+    def __init__(self, xmax, ymax, start_point, end_point, polygon_list):
+        self.E = Environment(xmax, ymax, start_point, end_point, polygon_list)
+        self.start_point = start_point
+        self.end_point = end_point
+        self.path = []
+        self.fre = {}
+        self.trace = {}
+
+    def output(self):
+        pass
+
+    def run(self):
+        pass
+    def total_cost(self):
+        pass
+    def imitate_environment(self):
+        self.path = self.output()
+        self.E.draw_environment()
+        if (len(self.path) != 0):
+            self.E.draw_path(self.path)
+        self.E.end_draw()
